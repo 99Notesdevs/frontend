6b139d4d05a5490c817e6edd8405348a4a2a6738
Ritik-Gupta-675 <ritikguptawork675@gmail.com>
2025-03-24 19:42:44 +0530

    Resolved merrge con

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,20 +2,23 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout/layout";
 import { useRouter } from "next/router";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
-  const isUser = router.pathname.startsWith('/users');
   const isDashboard = router.pathname.startsWith('/dashboard');
-
-  if (isUser || isDashboard) {
-    return <Component {...pageProps} />;
-  }
-
-
+  const isLogin = router.pathname.startsWith('/login');
+  const isUser = router.pathname.startsWith('/users');
+  
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ErrorBoundary>
+      {isDashboard || isUser || isLogin   ? (
+        <Component {...pageProps} />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </ErrorBoundary>
   );
 }
