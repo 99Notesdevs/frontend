cee51b73db66c278abcacb467ce512ffca2c8376
Manvir Singh <168892034+manvirsingh1716@users.noreply.github.com>
2025-04-16 18:07:35 +0530

    design

--- src/components/Blogs/BlogCard.tsx
+++ src/components/Blogs/BlogCard.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 interface Blog {
   id: string;
   title: string;
-  excerpt: string;
-  publishedDate: string;
+  createdAt: Date;
   slug: string;
+  metadata: string;
+  imageUrl: string;
 }
 
 interface BlogCardProps {
@@ -15,20 +17,37 @@ interface BlogCardProps {
 
 const BlogCard: React.FC<BlogCardProps> = ({ blog }) => {
   return (
-    <div className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 p-6">
-      <h3 className="text-xl font-semibold text-gray-800 mb-2">{blog.title}</h3>
-      <p className="text-gray-600 mb-4">{blog.excerpt}</p>
-      <div className="flex items-center justify-between">
-        <span className="text-sm text-gray-500">{new Date(blog.publishedDate).toLocaleDateString()}</span>
-        <Link
-          href={`/blogs/${blog.slug.replace(/\s+/g, '-')}`} // Convert spaces to hyphens
-          className="text-blue-600 hover:text-blue-800 font-medium"
-        >
-          Read More
-        </Link>
+    <Link href={`/blogs/${blog.slug}`}>
+    <div className="bg-white rounded-xl shadow-md hover:shadow-lg transition-all duration-300 h-[360px] flex flex-col">
+      <div className="relative h-[160px] w-full overflow-hidden">
+        <Image
+          src={blog.imageUrl}
+          alt={blog.title}
+          fill
+          className="object-cover"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw"
+        />
+      </div>
+      <div className="p-4 flex flex-col flex-grow">
+        
+        <h3 className="text-lg font-semibold text-gray-800 mb-2 line-clamp-2 h-[32px]">
+          {blog.title}
+        
+        </h3>
+        
+        <div className="flex items-center justify-between flex-grow">
+          <span className="text-sm text-gray-500">{new Date(blog.createdAt).toLocaleDateString()}
+          </span>
+          
+          <div className="line-clamp-2 h-[32px]">
+            {JSON.parse(blog.metadata)?.metaDescription}
+          </div>
+        </div>
       </div>
     </div>
+  </Link>
   );
 };
+
 
 export default BlogCard;
