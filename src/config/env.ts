c29222e4d2153a8aded661673810608cf5c870f4
im99N <devops.99notes@gmail.com>
2025-04-20 18:48:55 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-    // API: "http://13.49.244.55:5000/api/v1" // AWS api
-    API: "http://localhost:5000/api/v1", // Local api
+    API: "http://52.66.83.119:5000/api/v1" // AWS api
+    //API: "http://localhost:5000/api/v1", // Local api
     // API: "https://.../api/v1" // PROD api
 }
