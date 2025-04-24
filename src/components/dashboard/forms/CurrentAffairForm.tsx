011f2f1972e04482cce1fe683716d5edcb94e569
Manvir Singh <168892034+manvirsingh1716@users.noreply.github.com>
2025-04-25 00:15:44 +0530

    alerts

--- src/components/dashboard/forms/CurrentAffairForm.tsx
+++ src/components/dashboard/forms/CurrentAffairForm.tsx
@@ -93,6 +93,7 @@ export function CurrentAffairForm({ onSubmit, defaultValues }: CurrentAffairForm
       twitterImage: "",
       canonicalUrl: "",
       schemaData: "",
+      showInNav: false,
     },
   });
 
