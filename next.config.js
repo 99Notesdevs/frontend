37cc71dbbcd8091afb14496b258fb2bd2bf4c8bd
Vansh Maurya <vanshmaurya17@gmail.com>
2025-05-02 13:53:42 +0530

    Redirects only in production

--- next.config.js
+++ next.config.js
@@ -1,31 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/blog",
-  //       permanent: true, // Set to true for a 308 permanent redirect
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    // Only apply the redirect in production environment
+    if (process.env.NODE_ENV === "production") {
+      return [
+        {
+          source: "/",
+          destination: "/blog",
+          permanent: true, // Set to true for a 308 permanent redirect
+        },
+      ];
+    }
+    return []; // No redirects in production
+  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: '99notes-test.s3.eu-north-1.amazonaws.com',
-        pathname: '/**', // Allow all paths under this hostname
+        protocol: "https",
+        hostname: "99notes-test.s3.eu-north-1.amazonaws.com",
+        pathname: "/**", // Allow all paths under this hostname
       },
       {
-        protocol: 'https',
-        hostname: 'www.google.com',
-        pathname: '/**', // Allow all paths under this hostname
+        protocol: "https",
+        hostname: "www.google.com",
+        pathname: "/**", // Allow all paths under this hostname
       },
       {
-        protocol: 'https',
-        hostname: 'www.psdstack.com',
-        pathname: '/**', // Allow all paths for placeholder images
+        protocol: "https",
+        hostname: "www.psdstack.com",
+        pathname: "/**", // Allow all paths for placeholder images
       },
     ],
   },
