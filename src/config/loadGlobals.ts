12c0b68d577f9a05a63cd48efde2812a8f6dbd63
Vansh Maurya <vanshmaurya17@gmail.com>
2025-05-24 16:24:28 +0530

    loadGlobals with practiceQuestions

--- src/config/loadGlobals.ts
+++ src/config/loadGlobals.ts
@@ -4,11 +4,12 @@ const loadGlobals = async () => {
   try {
     const response = await fetch(`${env.API}/admin/ops`);
     const data = await response.json();
-    const { globalHeadScripts, globalBodyScripts, globalCss } = data.data;
+    const { globalHeadScripts, globalBodyScripts, globalCss, practiceQuestions } = data.data;
 
     const bodyScripts = globalBodyScripts[0]?.split('||')?.map((script: string) => script.trim());
     const headScripts = globalHeadScripts[0]?.split('||')?.map((script: string) => script.trim());
-    console.log('Head Scripts:', headScripts);
+    localStorage.setItem('practiceQuestions', practiceQuestions);
+    console.log(data.data)
 
     // Add scripts to the head
     if (headScripts && Array.isArray(headScripts)) {
